130956bd113da0fbb8bfd0550573684e36152840
kulchub-Aleksandr <kulchub@gmail.com>
2025-11-25 11:11:10 +0500

    Update Main.java 5

--- src/main/java/ru/kulchub/Main.java
+++ src/main/java/ru/kulchub/Main.java
@@ -3,7 +3,7 @@ package ru.kulchub;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    private static String msg = "Hello and welcome in Study!!"
+    private static String msg = "Hello and welcome"
     public static void main(String... args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
